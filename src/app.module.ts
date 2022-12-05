4d1ee2de64b9b23b42b1be5a456d79917182a80b
agus1-acosta <aguz.271976@gmail.com>
2022-12-05 17:16:50 -0300

    agregando servicios y controlador de empleados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsService } from './products/products.service';
 import { ProductsController } from './products/products.controller';
+import { EmployeeController } from './employee/employee.controller';
+import { EmployeeService } from './employee/employee.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [AppController, ProductsController, EmployeeController],
+  providers: [AppService, ProductsService, EmployeeService],
 })
 export class AppModule {}
